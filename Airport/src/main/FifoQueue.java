573097a3befbcd400cd38ba9e04d8421e56bfeaa
Thomas <thomgreimel@gmail.com>
2021-06-01 10:35:25 +0200

    dummy fifo queue

---
+++ Airport/src/main/FifoQueue.java
@@ -0,0 +1,25 @@
+package main;
+
+public class FifoQueue<T> {
+
+	public FifoQueue() {
+		
+	}
+	
+	public void enqueue(T v) {
+		
+	}
+	
+	public T dequeue() {
+		return null;
+	}
+	
+	public boolean empty() {
+		return false;
+	}
+	
+	public boolean full() {
+		return false;
+	}	
+	
+}
