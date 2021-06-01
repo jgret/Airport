723d189c8c637a9c73241303cf8e05b7e59bd8e9
Thomas <thomgreimel@gmail.com>
2021-06-01 10:56:59 +0200

    queues

--- Airport/src/main/Airport.java
+++ Airport/src/main/Airport.java
@@ -2,14 +2,42 @@ package main;
 
 import desmoj.core.simulator.Model;
 import desmoj.core.simulator.ProcessQueue;
+import java.util.ArrayList;
 
 public class Airport extends Model {
 	
+	/**
+	 * Queue for people on first Terminal waiting for a Bus to the car rent
+	 */
+
 	protected ProcessQueue<Human> terminalQueue1;
+
+	/**
+	 * Queue for people on second Terminal waiting for a Bus to the car rent
+	 */
+	
 	protected ProcessQueue<Human> terminalQueue2;
+
+	/**
+	 * Queue for people driving in the Bus
+	 */
+	
+	protected ArrayList<Human> busQueue;
+	
+	/**
+	 * people waiting at the car rent for a car
+	 */
+
+	protected ProcessQueue<Human> carRentQueue;
+	
+	/**
+	 * cars waiting at the car rent for people
+	 */
+
 	protected ProcessQueue<Car> carWaitQueue;
 	
-
+	protected Bus bus;
+	
 	public Airport(Model owner, String name, boolean showInReport, boolean showInTrace) {
 		super(owner, name, showInReport, showInTrace);
 	}
@@ -21,12 +49,12 @@ public class Airport extends Model {
 
 	@Override
 	public void doInitialSchedules() {
-		
+
 	}
 
 	@Override
 	public void init() {
-		
+
 	}
 
 }
