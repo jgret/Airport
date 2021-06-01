6f56c3c048660c4a7854d1bd765aad9326bbd0c6
Thomas <thomgreimel@gmail.com>
2021-06-01 10:21:11 +0200

    setup

---
+++ Airport/src/main/Airport.java
@@ -0,0 +1,26 @@
+package main;
+
+import desmoj.core.simulator.Model;
+
+public class Airport extends Model {
+
+	public Airport(Model owner, String name, boolean showInReport, boolean showInTrace) {
+		super(owner, name, showInReport, showInTrace);
+	}
+
+	@Override
+	public String description() {
+		return null;
+	}
+
+	@Override
+	public void doInitialSchedules() {
+		
+	}
+
+	@Override
+	public void init() {
+		
+	}
+
+}
---
+++ Airport/src/main/Runner.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import desmoj.core.util.AccessPoint;
+import desmoj.core.util.SimRunListener;
+import desmoj.extensions.experimentation.ui.ExperimentStarterApplication;
+import desmoj.extensions.experimentation.ui.GraphicalObserverContext;
+import desmoj.extensions.experimentation.ui.HistogramPlotter;
+import desmoj.extensions.experimentation.ui.TimeSeriesPlotter;
+import desmoj.extensions.experimentation.util.AccessUtil;
+import desmoj.extensions.experimentation.util.ExperimentRunner;
+
+
+/**
+ * @author Nicolas Knaak
+ * @author Philip Joschko
+ *
+ * A demo experiment runner
+ */
+public class Runner extends ExperimentRunner {
+	
+	public Runner() {
+		super(); 
+	}
+	
+	public Runner(Airport m) {
+		super(m);
+	}
+	
+	@Override
+	public SimRunListener[] createSimRunListeners(GraphicalObserverContext c) {
+		Airport model = (Airport)getModel();
+//		TimeSeriesPlotter tp1 = new TimeSeriesPlotter("Trucks",c, model.trucksArrived, 360,360);
+//		tp1.addTimeSeries(model.trucksServiced);
+//		HistogramPlotter hp = new HistogramPlotter("Truck Wait Times", c, model.waitTimeHistogram,"h", 360,360, 365,0);
+		return new SimRunListener[] {};
+	}
+	
+	@Override
+	public Map<String,AccessPoint> createParameters() {
+		Map<String,AccessPoint> pm = super.createParameters();
+		AccessUtil.setValue(pm, EXP_STOP_TIME, 1500.0);
+		AccessUtil.setValue(pm, EXP_TRACE_STOP, 100.0);
+		AccessUtil.setValue(pm, EXP_REF_UNIT, TimeUnit.MINUTES);
+		return pm;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new ExperimentStarterApplication(Airport.class, Runner.class).setVisible(true);
+	}
+}
