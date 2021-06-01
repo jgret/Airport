f26dab9359b064a50d366abe4e6f333c73b0a9a3
Icescrum-HofP <Spar2019!1>
2021-06-01 10:59:35 +0200

    Bus

--- Airport/src/main/Bus.java
+++ Airport/src/main/Bus.java
@@ -3,6 +3,7 @@ package main;
 import co.paralleluniverse.fibers.SuspendExecution;
 import desmoj.core.simulator.Model;
 import desmoj.core.simulator.SimProcess;
+import desmoj.core.simulator.TimeSpan;
 
 public class Bus extends SimProcess {
 
@@ -16,12 +17,27 @@ public class Bus extends SimProcess {
 
     public Bus(Model owner, String name, boolean showInTrace,int size,int waitingtime) {
         super(owner, name, showInTrace);
+        this.size = size;
+        this.time = waitingtime;
     }
 
     @Override
     public void lifeCycle() throws SuspendExecution {
+            //the servicer is always on duty and will never stop working
+            while (true) {
 
+
+
+
+            }
+    }
+
+
+    private void process(){
 
 
     }
+
+
+
 }
