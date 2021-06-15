f1b2c9c17d3e2bd54d6693102cb1b4053c73637a
Icescrum-HofP <Spar2019!1>
2021-06-15 10:22:32 +0200

    Bus wait update

--- Airport/src/main/Bus.java
+++ Airport/src/main/Bus.java
@@ -10,96 +10,116 @@ import desmoj.core.simulator.TimeSpan;
 
 public class Bus extends SimProcess {
 
-	private Airport airport;
+    private Airport airport;
 
-	public Bus(Model owner, String name, boolean repeating, boolean showInTrace) {
-		super(owner, name, repeating, showInTrace);
-		this.airport = (Airport) owner;
-	}
+    public Bus(Model owner, String name, boolean repeating, boolean showInTrace) {
+        super(owner, name, repeating, showInTrace);
+        this.airport = (Airport) owner;
+    }
 
-	@Override
-	public void lifeCycle() throws SuspendExecution {
+    @Override
+    public void lifeCycle() throws SuspendExecution {
+        boolean changesize = false;
+        TimeInstant start;
+        TimeInstant ende;
+        while (true) {
 
-		while (true) {
-			
-			TimeInstant start = presentTime();
-			TimeInstant ende = presentTime();
+            start = presentTime();
+            ende = presentTime();
 
-			while (airport.busQueue.size() <= 0 && (ende.getTimeRounded() - start.getTimeRounded()) <= 300) {
+            while (!changesize && (ende.getTimeRounded() - start.getTimeRounded()) <= 300) {
 
-				if (!airport.peopleWaitForBus.isEmpty()) {
+                if (!airport.peopleWaitForBus.isEmpty()) {
 
-					while (!airport.peopleWaitForBus.isEmpty() && airport.busQueue.size() < airport.busSize) {
-						Person person = airport.peopleWaitForBus.first();
-						airport.peopleWaitForBus.remove(person);
+                    while (!airport.peopleWaitForBus.isEmpty() && airport.busQueue.size() < airport.busSize) {
+                        Person person = airport.peopleWaitForBus.first();
+                        airport.peopleWaitForBus.remove(person);
 
-						hold(new TimeSpan(2));
+                        hold(new TimeSpan(2));
 
-						airport.busQueue.insert(person);
-					}
-				}
-				hold(new TimeSpan(1));
-				ende = presentTime();
-			}
-			airport.dataPeopleInBus.update(airport.busQueue.size());
-			
-			hold(new TimeSpan(200));
-			
-			// terminal 1
+                        airport.busQueue.insert(person);
+                    }
+                    changesize = true;
+                }
+                hold(new TimeSpan(1));
+                ende = presentTime();
+            }
+            airport.dataPeopleInBus.update(airport.busQueue.size());
 
-			for (Person p : airport.busQueue) {
-				if (p.getDestination() == Airport.DEST_TERMINAL_1) {
-					airport.busQueue.remove(p);
-					airport.dataPeopleTerminal1.update(++airport.totalTerminal1);
-					airport.dataPeopleInBus.update(airport.busQueue.size());
-					hold(new TimeSpan(2));
-				}
-			}
+            hold(new TimeSpan(200));
 
-			while (!airport.terminalQueue1.isEmpty() && airport.busQueue.size() < airport.busSize) {
-				Person person = airport.terminalQueue1.first();
-				airport.terminalQueue1.remove(person);
-				hold(new TimeSpan(2));
+            // terminal 1
+            changesize = false;
+            start = presentTime();
+            ende = presentTime();
 
-				airport.busQueue.insert(person);
-				airport.dataPeopleInBus.update(airport.busQueue.size());
-			}
+            while (!changesize && (ende.getTimeRounded() - start.getTimeRounded()) <= 300) {
+                for (Person p : airport.busQueue) {
+                    if (p.getDestination() == Airport.DEST_TERMINAL_1) {
+                        airport.busQueue.remove(p);
+                        airport.dataPeopleTerminal1.update(++airport.totalTerminal1);
+                        airport.dataPeopleInBus.update(airport.busQueue.size());
+                        hold(new TimeSpan(2));
+                        changesize = true;
+                    }
+                }
 
-			// terminal 2
+                while (!airport.terminalQueue1.isEmpty() && airport.busQueue.size() < airport.busSize) {
+                    Person person = airport.terminalQueue1.first();
+                    airport.terminalQueue1.remove(person);
+                    hold(new TimeSpan(2));
 
-			hold(new TimeSpan(20));
+                    airport.busQueue.insert(person);
+                    airport.dataPeopleInBus.update(airport.busQueue.size());
+                    changesize = true;
+                }
+                hold(new TimeSpan(1));
+                ende = presentTime();
+            }
+            // terminal 2
+            changesize = false;
+            start = presentTime();
+            ende = presentTime();
+            hold(new TimeSpan(20));
 
-			for (Person p : airport.busQueue) {
-				if (p.getDestination() == Airport.DEST_TERMINAL_2) {
-					airport.busQueue.remove(p);
-					airport.dataPeopleTerminal2.update(++airport.totalTerminal2);
-					airport.dataPeopleInBus.update(airport.busQueue.size());
-					hold(new TimeSpan(2));
-				}
-			}
+            while (!changesize && (ende.getTimeRounded() - start.getTimeRounded()) <= 300) {
+                for (Person p : airport.busQueue) {
+                    if (p.getDestination() == Airport.DEST_TERMINAL_2) {
+                        airport.busQueue.remove(p);
+                        airport.dataPeopleTerminal2.update(++airport.totalTerminal2);
+                        airport.dataPeopleInBus.update(airport.busQueue.size());
+                        changesize = true;
+                        hold(new TimeSpan(2));
+                    }
+                }
 
-			while (!airport.terminalQueue2.isEmpty() && airport.busQueue.size() < airport.busSize) {
-				Person person = airport.terminalQueue2.first();
-				airport.terminalQueue2.remove(person);
-				hold(new TimeSpan(2));
+                while (!airport.terminalQueue2.isEmpty() && airport.busQueue.size() < airport.busSize) {
+                    Person person = airport.terminalQueue2.first();
+                    airport.terminalQueue2.remove(person);
+                    hold(new TimeSpan(2));
 
-				airport.busQueue.insert(person);
-				airport.dataPeopleInBus.update(airport.busQueue.size());
-			}
+                    airport.busQueue.insert(person);
+                    airport.dataPeopleInBus.update(airport.busQueue.size());
+                    changesize = true;
+                }
+                hold(new TimeSpan(1));
+                ende = presentTime();
+            }
 
-			hold(new TimeSpan(200));
+            hold(new TimeSpan(200));
 
-			// car rent
-
-			for (Person p : airport.busQueue) {
+            // car rent
+            changesize = false;
+            if (airport.busQueue.size() > 0) {
+                for (Person p : airport.busQueue) {
 //				airport.peopleWaitForBus.insert(p);
-				airport.dataPeopleCarRent.update(++airport.totalCarRent);
-				hold(new TimeSpan(2));
-			}
-			airport.busQueue.removeAll();
-			airport.dataPeopleInBus.update(airport.busQueue.size());
-		}
-
-	}
-
+                    airport.dataPeopleCarRent.update(++airport.totalCarRent);
+                    hold(new TimeSpan(2));
+                }
+                airport.busQueue.removeAll();
+                airport.dataPeopleInBus.update(airport.busQueue.size());
+                changesize = true;
+            }
+        }
+    }
 }
